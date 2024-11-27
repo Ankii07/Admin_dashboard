668c59eebadcd81288ff488864c76f06f02295d9
Ankii07 <108866932+Ankii07@users.noreply.github.com>
2024-11-27 20:48:20 +0530

    admin_dashboard

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+  return (
+    <SettingSection icon={User} title={"Profile"}>
+      <div className="flex flex-col sm:flex-row items-center mb-6">
+        <img
+          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D"
+          alt="Profile"
+          className="rounded-full w-20 h-20 object-cover mr-4"
+        />
+
+        <div>
+          <h3 className="text-lg font-semibold text-gray-100">John Doe</h3>
+          <p className="text-gray-400">john.doe@example.com</p>
+        </div>
+      </div>
+
+      <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
+        Edit Profile
+      </button>
+    </SettingSection>
+  );
+};
+export default Profile;
